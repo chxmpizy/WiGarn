717858e9e598852b29c7bffdb86baea595418722
champNewbie <rattasat0012@gmail.com>
2025-09-02 20:45:13 +0700

    feat: add shadcn and i18n in web folder

---
+++ apps/web/app/src/shared/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@ui/button'
+import { Avatar,AvatarImage , AvatarFallback } from '@ui/avatar'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Button className='bg-red-400'>Click me</Button>
+    </div>
+  )
+}
+
+export default Navbar
