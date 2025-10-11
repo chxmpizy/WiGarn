1a307b092c839c2f0f1b298743e41ff3f5b31c5c
champNewbie <rattasat0012@gmail.com>
2025-10-11 16:38:11 +0700

    feat: initial login and register page

---
+++ apps/web/app/[locale]/(client)/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from '@/app/src/features/client/Auth/login/pages';
+
+const page = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default page;
---
+++ apps/web/app/[locale]/(client)/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterPage from '@/app/src/features/client/Auth/register/pages'
+
+const page = () => {
+  return (
+    <div>
+      <RegisterPage />
+    </div>
+  )
+}
+
+export default page
