9493e5a6e816dfe63d9ab9cf1438cf34e9e81c7b
champNewbie <rattasat0012@gmail.com>
2025-10-25 16:20:20 +0700

    feat(docker): add dockerfile and docker-compose

--- apps/api/src/module/auth/auth.module.ts
+++ apps/api/src/module/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt-strategy';
 import { PassportModule } from '@nestjs/passport';
-import { GoogleStrategy } from './strategies/google.strategy';
+// import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PassportModule.register({}),
     JwtModule.register({
       secret: process.env.JWT_SECRET!,
@@ -15,6 +19,10 @@ import { GoogleStrategy } from './strategies/google.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, JwtStrategy],
+  providers: [
+    AuthService,
+    // GoogleStrategy,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
--- apps/api/src/module/auth/strategies/google.strategy.ts
+++ apps/api/src/module/auth/strategies/google.strategy.ts
@@ -1,42 +1,42 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
-import { Injectable } from '@nestjs/common';
+// import { PassportStrategy } from '@nestjs/passport';
+// import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+// import { Injectable } from '@nestjs/common';
 
-interface Profile {
-  id: string;
-  displayName: string;
-  name?: {
-    familyName?: string;
-    givenName?: string;
-  };
-  emails?: Array<{ value: string }>;
-  photos?: Array<{ value: string }>;
-}
+// interface Profile {
+//   id: string;
+//   displayName: string;
+//   name?: {
+//     familyName?: string;
+//     givenName?: string;
+//   };
+//   emails?: Array<{ value: string }>;
+//   photos?: Array<{ value: string }>;
+// }
 
-@Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
-    super({
-      clientID: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
-      scope: ['email', 'profile'],
-    });
-  }
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: Profile,
-    done: VerifyCallback,
-  ) {
-    const email = profile.emails?.[0]?.value ?? '';
-    const name = profile.name?.givenName ?? profile.displayName ?? '';
-    const picture = profile.photos?.[0]?.value ?? '';
-    const user = {
-      email,
-      name,
-      picture,
-    };
-    done(null, user);
-  }
-}
+// @Injectable()
+// export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+//   constructor() {
+//     super({
+//       clientID: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//       callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+//       scope: ['email', 'profile'],
+//     });
+//   }
+//   async validate(
+//     accessToken: string,
+//     refreshToken: string,
+//     profile: Profile,
+//     done: VerifyCallback,
+//   ) {
+//     const email = profile.emails?.[0]?.value ?? '';
+//     const name = profile.name?.givenName ?? profile.displayName ?? '';
+//     const picture = profile.photos?.[0]?.value ?? '';
+//     const user = {
+//       email,
+//       name,
+//       picture,
+//     };
+//     done(null, user);
+//   }
+// }
